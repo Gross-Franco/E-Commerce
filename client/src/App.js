4a327fba73da53fbdabc79bd0e2ed79b55b045de
Abdias <abdiasejr@gmail.com>
2022-02-15 20:29:46 -0300

    rutas modularizadas a routes/routes.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import './App.css';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'
-import LandingPage from './containers/LandingPage';
-import NavBar from './containers/NavBar';
-import Home from './containers/Home';
-import Productos from './containers/Productos';
-import Blog from './containers/Blog';
-import Contactar from './containers/Contactar';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+import Routes from './routes/Routes';
 
 function App() {
 
   return (
-    <div className="App">
-      <NavBar/>
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/productos" element={<Productos/>} />
-        <Route exact path="/blog" element={<Blog/>} />
-        <Route exact path="/contactar" element={<Contactar/>} />
-      </Routes>
-    </div>
+    <>
+      <Provider store={store}>
+        <React.StrictMode>
+          <Routes />
+        </React.StrictMode>
+      </Provider>
+    </>
   );
 
 }
