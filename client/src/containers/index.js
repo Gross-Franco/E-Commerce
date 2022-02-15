e97e38a96cd480b9512883ff6cb2512b80c510e2
Abdias <abdiasejr@gmail.com>
2022-02-15 20:35:52 -0300

    helper para exportar todos los componentes que de momento utilicen

---
+++ client/src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as LandingPage } from './LandingPage';
+export { default as NavBar } from './NavBar';
+export { default as Home } from './Home';
+export { default as Productos } from './Productos';
+export { default as Blog } from './Blog';
+export { default as Contactar } from './Contactar';
