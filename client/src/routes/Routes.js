d10b63a787d31b27d447fc33607570d13ef7e858
Abdias <abdiasejr@gmail.com>
2022-02-15 20:48:54 -0300

    rutas ahora con el login y el registro

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, Productos, Blog, Contactar, NavBar } from "../containers";
+import { Home, Productos, Blog, Contactar, NavBar, Login, Registro } from "../containers";
 
 const Rutas = () => {
   return (
@@ -11,6 +11,8 @@ const Rutas = () => {
         <Route path="/productos" element={<Productos />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contactar" element={<Contactar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
       </Routes>
     </Router>
   );
