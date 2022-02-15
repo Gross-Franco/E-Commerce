41e2ed14a3ee37c3592e50b80c9d098c8c00910c
Erick <fis9.81@gmail.com>
2022-02-15 17:48:55 -0400

    se actualizo react-scripts a la vercion 5 para prevenir futuros bugs y se agrego las pestañas de home, productos, blog y contactar

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
+import Rutas from "../components/Rutas";
 import './estilos/navBar.css'
 
 export default function NavBar(){
@@ -7,6 +8,7 @@ export default function NavBar(){
     return(
         <div className="navbar">
             <h1>aqui va el tittulo</h1>
+            <Rutas/>
             <SearchBar/>
         </div>
     )
