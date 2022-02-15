f0ac109552484fa56c90147d34ac6929d422f075
f2000fernandez <f2000.fernandez@gmail.com>
2022-02-15 14:44:41 -0300

    small fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React from 'react';
 import { Route } from 'react-router-dom';
+import LandingPage from './Components/LandingPage';
 
 
 function App() {
   
   return (
     <div className="App">
-      <Route exact path="/" render={() => <Landing/>} />
+      <Route exact path="/" render={() => <LandingPage/>} />
     </div>
   );
 
