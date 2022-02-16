9eb0d22c27bb3bb49058cff9ea665921a99f3536
Abdias <abdiasejr@gmail.com>
2022-02-16 11:59:43 -0300

    cambio de archivo ahora es jsx

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+export default function SearchBar(){
+    const [busqueda, setBusqueda] = React.useState('');
+
+    return(
+        <div className="busqueda">
+            <form onSubmit={e=>{
+                e.preventDefault()
+                alert(busqueda);
+            }}>
+                <input type='text' placeholder="buscar ..."
+                    onChange={e=>setBusqueda(e.target.value)}
+                />
+                <input type='submit' value='buscar'/>
+            </form>
+        </div>
+    )
+}
