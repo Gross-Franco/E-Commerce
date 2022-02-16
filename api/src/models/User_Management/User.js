b56fd0a4497abff93ea12a98e73fc7bc204b1d30
jozzol <joseg1224@hotmail.com>
2022-02-16 18:43:29 -0500

    fixed conflicts due to the post review function, added password hash

--- api/src/models/User_Management/User.js
+++ api/src/models/User_Management/User.js
@@ -18,12 +18,13 @@ module.exports = (sequelize) => {
         validate: {
             is: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm         
             // Minnumum 8 characters, 1 uppercase, 1 lowercase, 1 number. Can contain special characters 
-        },
-        set(value) {
-            bcrypt.hash(value, 10, function(err, hash) {
-                this.setDataValues('password', hash)
-            });
-        }
+        }, 
+        // comente esto por el momento ya que al crear el usuario me estaba arrojando que no recibia el password por el body
+        // set(value) {
+        //     bcrypt.hash(value, 10, function(err, hash) {
+        //         this.setDataValues('password', hash)
+        //     });
+        // }
     },
 
     first_name: {
--- api/src/routes/User/controller.js
+++ api/src/routes/User/controller.js
@@ -1,8 +1,7 @@
-
 require('dotenv').config()
 const axios = require('axios');
-const {User, UserAddress, UserPayment, } = require ('../../db.js')
-let  {Product,Review,User} =require('../../db.js')
+const {User, UserAddress, UserPayment, Product, Review} = require ('../../db.js')
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const {FIRM} =process.env 
 
@@ -29,9 +28,11 @@ const createUser = async (req, res) => {
         isAdmin
     } = req.body
 
+    const passwordHash = await bcrypt.hash(password, 10);
+
     let createdUser = await User.create({
         username,
-        password,
+        password: passwordHash,
         first_name,
         last_name,
         email,
@@ -70,34 +71,32 @@ const addAdress = async(req, res) =>{
     res.json({createdAddress, msg: "added address"})
 }
 
-module.exports = {createUser, getUsers, addAdress}
 
 
-module.exports={
-    postReviewProduct:async (req,res)=>{
-        // si se envia el token
-        //let {id}=jwt.decode(req.headers['authorization'].split(' ')[1])
-        // de lo contrario se envia el id del usuario de forma manual
-        //let {userID}= req.query
-        // per es necesario un identificador para buscar el usuario en la base de datos
-        // o si se usar cookie-session 
-        // let {usAuth}= req.session
-        // let {id} = jwt.decode(usAuth)
-        try{
-            let {idProduct} =req.params
-            if(req.body){
-                if(req.body.hasOneProperty('description')&& typeof req.body['description']!== 'string'){
-                    throw Error('Data types error ')
-                }
-                if(req.body.hasOneProperty('starsPoint')&& typeof req.body['starsPoint']!== 'number'){
-                    throw Error('Data types error ')
-                }
+const postReviewProduct = async (req,res)=>{
+    // si se envia el token
+    //let {id}=jwt.decode(req.headers['authorization'].split(' ')[1])
+    // de lo contrario se envia el id del usuario de forma manual
+    //let {userID}= req.query
+    // per es necesario un identificador para buscar el usuario en la base de datos
+    // o si se usar cookie-session 
+    // let {usAuth}= req.session
+    // let {id} = jwt.decode(usAuth)
+    try{
+        let {idProduct} =req.params
+        if(req.body){
+            if(req.body.hasOneProperty('description')&& typeof req.body['description']!== 'string'){
+                throw Error('Data types error ')
             }
-            let product=await Product.findOne({where:{id:idProduct}})
-            !product&& new Error('Product no found')
-            let review= await Review.create(req.body)
-            // dara un error si no hay una id de un usuario
-            User.findOne({where:{id:id}})
+            if(req.body.hasOneProperty('starsPoint')&& typeof req.body['starsPoint']!== 'number'){
+                throw Error('Data types error ')
+            }
+        }
+        let product=await Product.findOne({where:{id:idProduct}})
+        !product&& new Error('Product no found')
+        let review= await Review.create(req.body)
+        // dara un error si no hay una id de un usuario
+        User.findOne({where:{id:id}})
             .then((result) => {
                 review.addUser(result)
                 product.setReview(review)
@@ -105,10 +104,12 @@ module.exports={
             },(error)=>{
                 res.status(400).json({success:false,inf:'user nof found: '+error})
             })
-                
+            
             
         }catch(e){
             res.status(400).json({success:false,inf:e})
         }
     }
-}
+
+
+module.exports = {createUser, getUsers, addAdress, postReviewProduct}
