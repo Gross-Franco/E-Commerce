d6b35d34e1d6a5ca5bbd1957a0dc3114e4efdce7
NessDisk <ness.cero1@gmail.com>
2022-02-16 00:58:32 -0500

    Home_Protipo
    -Add holderjs (permite agregar imagenes en una resolución de prueba )
    -Maquetado en bruto del Home
    - Agregados imagenes de prueba menu de desplazamiento.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 import Routes from "./routes/Routes";
-import './App.css';
+// import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -6,18 +6,28 @@ import React from "react";
  import Conntainer from 'react-bootstrap/Container';
  import Carousel from  'react-bootstrap/Carousel';
 
+ import Holder from 'react-holder'
+import Container from "react-bootstrap/Container";
+import Image from 'react-bootstrap/Image'
+import ButtonGroup   from 'react-bootstrap/ButtonGroup'
+
 
 export default function Home(){
 
+
+
+
     return(
         <div>
-            <h1>HOME</h1>
+            {/* <h1>HOME</h1> */}
+
+    
             <Conntainer>
             <Carousel>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
+      src="https://media.istockphoto.com/photos/asian-male-florist-owner-of-small-business-flower-shop-using-digital-picture-id1317277259"
       alt="First slide"
     />
     <Carousel.Caption>
@@ -28,7 +38,7 @@ export default function Home(){
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
+      src="https://media.istockphoto.com/photos/multiethnic-team-of-composers-working-in-studio-picture-id1168477456"
       alt="Second slide"
     />
 
@@ -40,7 +50,7 @@ export default function Home(){
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
+      src="https://media.istockphoto.com/photos/business-owner-flower-shop-picture-id925172618"
       alt="Third slide"
     />
 
@@ -52,11 +62,18 @@ export default function Home(){
 </Carousel>
 
 </Conntainer>
-            //---- // ---- //
+            
+
+         
+{
+
+    // ------------------------------
+}
+
             <Conntainer>
             <CardGroup>
   <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
+  <Card.Img variant="top" src="holder.js/100px300" />
     <Card.Body>
       <Card.Title>Card title</Card.Title>
       <Card.Text>
@@ -69,7 +86,7 @@ export default function Home(){
     </Card.Footer>
   </Card>
   <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Img variant="top" src="holder.js/100px300" />
     <Card.Body>
       <Card.Title>Card title</Card.Title>
       <Card.Text>
@@ -82,7 +99,21 @@ export default function Home(){
     </Card.Footer>
   </Card>
   <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Img variant="top" src="holder.js/100px300" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px300" />
     <Card.Body>
       <Card.Title>Card title</Card.Title>
       <Card.Text>
@@ -96,7 +127,105 @@ export default function Home(){
     </Card.Footer>
   </Card>
 </CardGroup>
+{
+
+// ------------------------------
+}
+<CardGroup>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px300" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px300" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px300" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px300" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
+
+
           </Conntainer>
+
+          <div/>
+          <br/>
+          <br/>
+          <Container>
+          
+
+    <Image variant="top" src="holder.js/100px300"/>
+    <ButtonGroup  >
+    <Button  className="sticky-top">Ver</Button>
+    {/* <Button className="position-absolute top-100 start-50">Middle</Button>
+    <Button className="position-absolute top-100 start-100">Right</Button> */}
+    </ButtonGroup> 
+    
+
+            
+            </ Container>
+
+            <div/>
+
+            <Container>
+          
+
+          <Image variant="top" src="holder.js/100px300"/>
+          <ButtonGroup  >
+          <Button  className="sticky-top">Ver</Button>
+          {/* <Button className="position-absolute top-100 start-50">Middle</Button>
+          <Button className="position-absolute top-100 start-100">Right</Button> */}
+          </ButtonGroup> 
+          
+      
+                  
+                  </ Container>
+
         </div>
+
+
     )
 }
