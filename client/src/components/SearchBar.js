109ed8685884820e401e2a13f1fe7067d012c1b1
Erick <fis9.81@gmail.com>
2022-02-15 20:01:54 -0400

    se agregaron los botones de login y registro y se modifico algo de css

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from "react";
 import './estilos/searchBar.css'
 export default function SearchBar(){
+    const [busqueda, setBusqueda] = React.useState('');
 
     return(
         <div className="busqueda">
             <form onSubmit={e=>{
                 e.preventDefault()
+                alert(busqueda);
             }}>
-                <input type='text' placeholder="buscar ..."/>
+                <input type='text' placeholder="buscar ..."
+                    onChange={e=>setBusqueda(e.target.value)}
+                />
                 <input type='submit' value='buscar'/>
             </form>
         </div>
