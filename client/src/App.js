4e7609808b869fbfea33c184c8eeddceaaff94ea
Abdias <abdiasejr@gmail.com>
2022-02-16 09:49:27 -0300

    line deleted

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 import Routes from "./routes/Routes";
-import './App.css';
 
 function App() {
   return (
