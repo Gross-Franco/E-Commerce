9df00eae7c05bc74ce00f9e9ca354ff5166e3c05
Abdias <abdiasejr@gmail.com>
2022-02-16 21:35:57 -0300

    /catalogo en vez de /productos

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, Productos, Header, Login, Registro, Contactar,Blog } from "../containers";
+import { Home, Catalog, Header, Login, Registro, Contactar,Blog } from "../containers";
 
 const Rutas = () => {
   return (
@@ -8,7 +8,7 @@ const Rutas = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/productos" element={<Productos />} />
+        <Route path="/catalogo" element={<Catalog />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contactar" element={<Contactar />} />
         <Route path="/registro" element={<Registro />} />
