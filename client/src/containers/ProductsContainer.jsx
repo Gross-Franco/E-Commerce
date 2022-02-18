89bc67770495824d2f346e0a3efddc32ace44ef7
Abdias <abdiasejr@gmail.com>
2022-02-18 11:15:32 -0300

    resolving conflicts

--- client/src/containers/ProductsContainer.jsx
+++ client/src/containers/ProductsContainer.jsx
@@ -1,24 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Products } from "../containers";
-import { commerce } from "../lib/commerce";
 
 const ProductsContainer = () => {
-  const [products, setProducts] = useState([]);
-
-  const fetchProducts = async () => {
-    const { data } = await commerce.products.list();
-    setProducts(data);
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  console.log(products);
   
   return (
     <div className="products--container">
-      <Products products={products} />
+      <Products />
     </div>
   );
 };
