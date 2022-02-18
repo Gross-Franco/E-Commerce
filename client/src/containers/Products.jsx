7e4dda32eb91b3b7d7f425733bd8f694a092dc05
f2000fernandez <f2000.fernandez@gmail.com>
2022-02-18 20:12:16 -0300

    added redux conection to backend in searchbar and products components

--- client/src/containers/Products.jsx
+++ client/src/containers/Products.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Grid } from "@mui/material";
 import p from "../helpers/mockProducts";
 import { Product } from "../components";
+import { getProducts } from "../Redux/Actions/actions";
 
 const Products = () => {
-  const { products } = p;
+  // const { products } = p;
+
+  const {products} = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(products.length === 0) dispatch(getProducts());
+  }, [])
+
   return (
     <main>
       <div>
