6a1c4daaf01a9f0be80d0eade57c706b1cd4a51e
Abdias <abdiasejr@gmail.com>
2022-02-19 16:28:50 -0300

    new cases to the reducer

--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -1,19 +1,48 @@
-import { GET_ALL_PRODUCTS, GET_BACKUP, GET_CATEGORIES, SEARCH_PRODUCT_ID, SEARCH_PRODUCT_NAME, DELETE_PRODUCT, CREATE_PRODUCT, FILTER_PRODUCTS, ORDER_PRODCTS} from './Actions/actionTypes'
+import {
+  GET_PRODUCTS,
+  GET_BACKUP,
+  GET_CATEGORIES,
+  SEARCH_PRODUCT_ID,
+  SEARCH_PRODUCT_NAME,
+  DELETE_PRODUCT,
+  CREATE_PRODUCT,
+  FILTER_PRODUCTS,
+  ORDER_PRODCTS,
+  GET_USER,
+} from "./Actions/actionTypes";
 
 const initialState = {
-    products: [],
-    productDetail: {},
-    UserTest:[]
+  products: [],
+  productDetail: {},
+  UserTest: [],
+  categories: [],
 };
 
-
-
 const rootReducer = (state = initialState, action) => {
-    switch(action.type) {
-            
-        default: 
-            return {...state}
-    };
-}
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+      };
+    case CREATE_PRODUCT:
+      return {
+        ...state,
+        products: [...state.products, action.payload],
+      };
+    case GET_USER: 
+      return {
+        ...state,
+        isAdmin: action.payload,
+      };
+    case GET_CATEGORIES: 
+      return {
+        ...state,
+        categories: action.payload,
+      };
+    default:
+      return { ...state };
+  }
+};
 
 export default rootReducer;
