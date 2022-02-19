19fe4456f5fe1a3aa5fa8bba44de8ed198e20c6e
Abdias <abdiasejr@gmail.com>
2022-02-19 16:26:16 -0300

    new actions types

--- client/src/Redux/Actions/actionTypes.js
+++ client/src/Redux/Actions/actionTypes.js
@@ -1,4 +1,4 @@
-export const GET_ALL_PRODUCTS = 'GET_ALL_RECIPES';
+export const GET_PRODUCTS = 'GET_RECIPES';
 export const GET_BACKUP = 'GET_BACKUP_RECIPES';
 export const SEARCH_PRODUCT_NAME = 'GET_RECIPE_NAME';
 export const SEARCH_PRODUCT_ID = 'GET_RECIPE';
@@ -7,3 +7,5 @@ export const GET_CATEGORIES = 'GET_DIETS';
 export const CREATE_PRODUCT = 'CREATE_RECIPE';
 export const FILTER_PRODUCTS = "FILTER_RECIPES";
 export const ORDER_PRODCTS = "ORDER_RECIPES";
+export const GET_USER = "GET_USER";
+export const CREATE_CATEGORY = "CREATE_CATEGORY";
