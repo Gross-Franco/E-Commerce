ba1c6af99123cfcbb508adca56648c13af6e1ac1
Abdias <abdiasejr@gmail.com>
2022-02-21 12:22:57 -0300

    fix conflicts

--- client/src/containers/Products.jsx
+++ client/src/containers/Products.jsx
@@ -1,17 +1,11 @@
 import React, {useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Grid } from "@mui/material";
-import p from "../helpers/mockProducts";
-<<<<<<< HEAD
 import { Product } from "../components";
 import { getProductsPublic } from "../Redux/Actions/actions";
 
-=======
-import { getProducts } from "../Redux/Actions/actions";
 import CardItemsHome from "./ItemsDisplayHome/CardItemsHome";
->>>>>>> fe3aa24a30d64ee5d053f2048829542ecd6c193c
 const Products = () => {
-  // const { products } = p;
 
   const {products} = useSelector((state) => state);
   const dispatch = useDispatch();
