a40dbfe665b76b789d059459fd5eef32784d03aa
Erick <fis9.81@gmail.com>
2022-02-21 12:09:18 -0400

    se cambiaron estilos de footer, (links)

--- client/src/containers/Footer.jsx
+++ client/src/containers/Footer.jsx
@@ -9,13 +9,13 @@ export default function Footer() {
             <footer className="text-white py-4 bg-dark">
                 <div>
                     <nav className="row">   
-                    <Link to="/" className="col-12 col-md-3 d-flex align-items-center justyfy-content-center"> LOGO DEL ecommerce</Link>
-                    <ul className="col-12 col-md-2 list-unstyled">
+                    <Link to="/" className="col-12 col-md-3 d-flex align-items-center justyfy-content-center" style={{ textDecoration: 'none', color:'white' }}> LOGO DEL ecommerce</Link>
+                    <ul className="col-12 col-md-2 list-unstyled" >
                         <li>Redes Sociales</li>
-                        <li><i className="bi bi-instagram"/><a href="https://www.instagram.com/">Instagram</a></li>
-                        <li><i className="bi bi-facebook" /><a href="https://www.facebook.com/">Facebook</a></li>
-                        <li><i className="bi bi-twitter"/><a href="https://www.twitter.com/">Twitter</a></li>
-                        <li><i className="bi bi-youtube"/><a href="https://www.youtube.com/">Youtube</a></li>                   
+                        <li><i className="bi bi-instagram"/><a href="https://www.instagram.com/" style={{ textDecoration: 'none', color:'white' }}>Instagram</a></li>
+                        <li><i className="bi bi-facebook" /><a href="https://www.facebook.com/" style={{ textDecoration: 'none', color:'white' }}>Facebook</a></li>
+                        <li><i className="bi bi-twitter"/><a href="https://www.twitter.com/" style={{ textDecoration: 'none', color:'white' }}>Twitter</a></li>
+                        <li><i className="bi bi-youtube"/><a href="https://www.youtube.com/" style={{ textDecoration: 'none', color:'white' }}>Youtube</a></li>                   
                     </ul>
                     <ul className="col-12 col-md-2 list-unstyled">
                         <li>Terminos y condiciones</li>
@@ -26,7 +26,7 @@ export default function Footer() {
                     <ul className="col-12 col-md-2 list-unstyled">
                         <li>Mi cuenta</li>
                         <li>login</li>
-                        <li><Link to="/registro">Registrarse</Link></li>
+                        <li><Link to="/registro" style={{ textDecoration: 'none', color:'white' }}>Registrarse</Link></li>
                     </ul>
                     </nav>
                 </div>
