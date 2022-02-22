e7c238f2edafd59ed4680c947b89c2d8462e90ef
f2000fernandez <f2000.fernandez@gmail.com>
2022-02-22 16:04:53 -0300

    modularizar admin router por fin

---
+++ api/src/routes/Admin/controllerCategories.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const { Op } = require("sequelize");
+
+const {
+  ProductCategory,
+} = require("../../db.js");
+  
+const getCategory = async (req, res) => {
+    let search = await ProductCategory.findAll();
+    let categories = search.map((x) => ({
+      name: x.name,
+      description: x.description,
+      id: x.id,
+    }));
+  
+    res.status(200).send(categories);
+};
+
+const searchCategoryName = async function (req, res) {
+    const { name } = req.query;
+    if (!name) {
+      return res.status(404).send("Invalid name");
+    }
+    try {
+      let categoriesByName = await ProductCategory.findAll({
+        where: {
+          name: {
+            [Op.iLike]: "%" + name + "%",
+          },
+        },
+      });
+      let finalres = categoriesByName.map((x) => ({
+        name: x.name,
+        description: x.description,
+        id: x.id,
+      }));
+      res.json(finalres);
+    } catch (err) {
+      console.log(err);
+      res.status(404).send(err);
+    }
+};
+
+const createCategory = async (req, res) => {
+    let { name, description } = req.body;
+  
+    let createdCategory = await ProductCategory.create({
+      name,
+      description,
+    });
+    res.json(createdCategory);
+};
+
+module.exports = {
+    getCategory,
+    searchCategoryName,
+    createCategory,
+}
