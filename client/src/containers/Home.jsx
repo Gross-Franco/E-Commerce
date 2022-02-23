52d80be6a836f5a72c5d39bdbba0250aa4c0756e
Abdias <abdiasejr@gmail.com>
2022-02-23 09:08:08 -0300

    ctrl + s

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -27,13 +27,8 @@ export default function Home() {
 
   const [ValueRandom, SetValueRandom] = useState([]); */
 
-<<<<<<< HEAD
-  let { products } = useSelector((state) => state);
-  const dispatch = useDispatch();
-=======
   let { products } = useSelector((state) => state.products);
   const dispatch = useDispatch()
->>>>>>> Developement
 
   useEffect(() => {
     dispatch(getProductsPublic());
