3b36d6ad226e059593fcbba8822f13b22da7e563
f2000fernandez <f2000.fernandez@gmail.com>
2022-02-24 12:41:19 -0300

    add getItems and minor changes to shopping routes

--- api/src/routes/Shoping/index.js
+++ api/src/routes/Shoping/index.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { shoppingSessionInit, addCartItem, shoppingTotalEdit, deleteShoppingSession, editItemQuantity, deleteCartItem, createOrder }  = require('./controller');
+const { shoppingSessionInit, deleteCart, addCartItem, getCartItems, shoppingTotalEdit, deleteShoppingSession, editItemQuantity, deleteCartItem, createOrder }  = require('./controller');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -11,9 +11,11 @@ router.post("/session", shoppingSessionInit);
 router.put("/session", shoppingTotalEdit);
 router.delete("/session", deleteShoppingSession);
 //cart items
+router.get("/cart", getCartItems);
 router.post("/cart", addCartItem);
 router.put("/cart", editItemQuantity);
-router.delete("/cart", deleteCartItem);
+router.delete('/cart', deleteCart)
+router.delete("/item", deleteCartItem);
 //purchase order
 router.post("/order", createOrder);
 
