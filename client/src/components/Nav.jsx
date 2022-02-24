14e541c1a2e3b79f48b5ef0153d05c4726f89e7a
Abdias <abdiasejr@gmail.com>
2022-02-23 22:47:52 -0300

    styles when window is scrolling, state to props

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export default function Nav() {
+export default function Nav({ isScroll }) {
   return (
     <div className="header-nav">
-      <NavLink className="header-nav--link" to="/">
+      <NavLink className={`header-nav--link ${isScroll ? 'scroll-link' : ''}`}to="/">
         Home
       </NavLink>
-      <NavLink className="header-nav--link" to="/catalogo">
+      <NavLink className={`header-nav--link ${isScroll ? 'scroll-link' : ''}`} to="/catalogo">
         Catalogo
       </NavLink>
     </div>
