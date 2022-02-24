5a1213a5ff4a6addb2c612ed21a4d129c5d26da2
NessDisk <ness.cero1@gmail.com>
2022-02-23 19:54:40 -0500

    Fixed_PP

--- api/index.js
+++ api/index.js
@@ -41,7 +41,7 @@ const { PORT } = process.env;
 // Syncing all the models at once.
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(PORT || 3001, () => {
     ProductInventory.bulkCreate(mockinventory).then(() => {
     Product.bulkCreate(mockproducts).then(() => {
       ProductCategory.bulkCreate(mockcategories).then(()=>{
@@ -61,8 +61,6 @@ conn.sync({ force: true }).then(() => {
       })
     })
     .catch(error => console.log(error));
-    server.listen(3001, () => {
-      console.log('%s listening at 3001'); // eslint-disable-line no-console
-    });
+    console.log(`%s listening at ${PORT || 3001} `); // eslint-disable-line no-console
   });
 });
