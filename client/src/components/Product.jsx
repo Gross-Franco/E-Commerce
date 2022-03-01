e64a9a04f4c8916df4dc77d3af01983568341243
Abdias <abdiasejr@gmail.com>
2022-03-01 14:16:55 -0300

    when is click is there is no session, save in localstorage

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,25 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { MdAddShoppingCart } from "react-icons/md";
 import { useSelector, useDispatch } from "react-redux";
-import { addCartItem } from "../Redux/Actions/actions";
+import { addCartItem, saveLocal } from "../Redux/Actions/actions";
+import { saveLocalStorage } from "../services";
 
 const Product = ({ product }) => {
-  const { session } = useSelector((state) => state.shopping);
+  const { session, cartStorage } = useSelector((state) => state.shopping);
   const dispatch = useDispatch();
   const handleClick = () => {
-    dispatch(addCartItem(session.id, product.id));
+    if (session.length > 0) {
+      dispatch(addCartItem(session.id, product.id));
+    } else {
+      saveLocalStorage({...product, quantity: 1});
+      dispatch(saveLocal());
+    }
   };
-  
+
   return (
     <div className="product--item">
       <Link to={`/product/${product.id}`} className="product--link">
-        <div className="product--image" style={{backgroundImage: `url(${product.image})`}} />
+        <div
+          className="product--image"
+          style={{ backgroundImage: `url(${product.image})` }}
+        />
         <p className="product--title"> {product.name} </p>
         <p className="product--info"> {product.description}</p>
       </Link>
       <div className="product--price-container">
         <p className="product--price"> $ {product.price} </p>
-        <MdAddShoppingCart className="product--cart-icon" onClick={handleClick}/>
+        <MdAddShoppingCart
+          className="product--cart-icon"
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
