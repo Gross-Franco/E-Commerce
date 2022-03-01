9f0ebc970a0caef5eb24e46c6b8d73d58c919ca6
Abdias <abdiasejr@gmail.com>
2022-03-01 14:24:02 -0300

    save in local service

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -5,3 +5,27 @@ export const setOverflowY = (isOpen) => {
 export const isFullfilled = (form) => {
   return Object.keys(form).some((key) => form[key] !== "");
 };
+
+export const saveLocalStorage = (product) => {
+  const cart = JSON.parse(window.localStorage.getItem("cartItems"));
+  let newCart = [];
+  if (!cart) {
+    newCart = [product];
+  } else {
+    newCart = cart.every((item) => item.id !== product.id)
+      ? [...cart, product]
+      : cart.map((item) => {
+          if (item.id === product.id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+  }
+  let subTotal = Number(
+    Math.round(
+      newCart.reduce((acc, item) => acc + item.price * item.quantity, 0) + "e2"
+    ) + "e-2"
+  );
+  window.localStorage.setItem("cartItems", JSON.stringify(newCart));
+};
