884a9c9242b8b5b90e98d8e5c963cadcad1a725a
Abdias <abdiasejr@gmail.com>
2022-03-01 14:22:23 -0300

    new export

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,3 +4,4 @@ export { default as PassReset } from './PassReset';
 export { default as Cart } from './Cart';
 export { default as Checkout } from './Checkout';
 export { default as Register } from './Register';
+export { default as Login } from './Login';
