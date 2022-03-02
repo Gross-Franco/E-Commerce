2655712d04bece897d82a5cb472dbd4dff9242fb
unknown <gross.franco@hotmail.com>
2022-03-02 15:51:05 -0300

    Client & server exchange cookies

---
+++ client/src/utilities/axios.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+// import { setAuthLevel } from "../Redux/Actions/actions";
+// import store from "../Redux/store"
+// require("dotenv").config();
+
+export const axiosWithCredentials = axios.create({
+    baseURL: process.env.REACT_APP_API,
+    withCredentials: true,
+})
+
+axiosWithCredentials.interceptors.response.use((response) => {
+    console.log(response.headers);
+    // if (response.data.isAdmin) {
+    //     store.dispatch(setAuthLevel(3));
+    // } else if (response.data.isUser) {
+    //     store.dispatch(setAuthLevel(2));
+    // } else if ((response.data.hasOwnProperty("isUser") && response.data.hasOwnProperty("isAdmin")) && (!response.data.isUser && !response.data.isAdmin)) {
+    //     store.dispatch(setAuthLevel(1));
+    // }
+    return response;
+}, (error) => {
+    console.log(error);
+    return error;
+})
+
+// axiosWithCredentials.interceptors.request.use((config) => {
+//     const url = config.url.split("/")[1];
+//     let level = 1;
+
+//     if (url === "admin") {
+//         level = 3;
+//     } else if (url === "user") {
+//         level = 2;
+//     }
+
+//     config.headers.common["Permits"] = `Level ${level}`;
+
+//     return config;
+// })
