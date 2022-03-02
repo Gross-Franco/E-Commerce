00cfda6a33aca88866f61d04cb6c6b0ab47ad53b
Abdias <abdiasejr@gmail.com>
2022-03-02 16:57:01 -0300

    defining base url in an better way

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import store from "./Redux/store";
 import Routes from "./routes/Routes";
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "https://pghenry.herokuapp.com";
+const { REACT_APP_DEV, REACT_APP_PRO, NODE_ENV } = process.env;
+axios.defaults.baseURL = NODE_ENV === 'development' ?  REACT_APP_DEV : REACT_APP_PRO;
+// npm start for dev mode and npm run build for production mode
+// set in .env file your base url for dev and production on client side root directory
+// pd: it should starts with REACT_APP_
 function App() {
 
 
