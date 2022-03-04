0a0bb53dfbd93bed9b1e18c37bc2c0ae9ac27d00
Abdias <abdiasejr@gmail.com>
2022-03-03 23:37:59 -0300

    refactoring inner text to show

--- client/src/helpers/formValidation/register.js
+++ client/src/helpers/formValidation/register.js
@@ -1,19 +1,21 @@
 import axios from "axios"
 const nameRe = /[a-zA-Z]+/;
 const emailRe = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-const passwordRe = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/m
+const passwordRe = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/m;
+const { REACT_APP_DEV, REACT_APP_PRO, NODE_ENV } = process.env;
+axios.defaults.baseURL = NODE_ENV === 'development' ?  REACT_APP_DEV : REACT_APP_PRO;
 
 const validateUsername = async (username) => {
     if(username.length === 0) return 'Please insert a valid username'
-    const existing = await axios.get(`/user/validate?username=${username}`);
-    if(existing.data) return 'Username is taken';
+    const response = await axios.get(`/user/validate?username=${username}`);
+    if(response.data) return 'Username is taken';
     return false;
 }
 
 const validateEmail = async (email) => {
     if(!emailRe.test(email)) return 'Please insert a valid Email'
-    const existing = await axios.get(`/user/validate?email=${email}`); 
-    if(existing.data) return "There's already an account registered with this email"
+    const response = await axios.get(`/user/validate?email=${email}`); 
+    if(response.data) return "There's already an account registered with this email"
     return false;
 }
 
@@ -24,11 +26,11 @@ const validatePassword = async (password) => {
 
 const validateFirstName = (firstName) => {
     if(nameRe.test(firstName)) return false
-    return 'Please insert a valid name' 
+    return 'Please insert a name' 
 }
 
 const validateLastName = (lastName) => {
-    if(!nameRe.test(lastName)) return 'Please insert a valid last name'
+    if(!nameRe.test(lastName)) return 'Please insert a last name'
     return false;
 }
 
