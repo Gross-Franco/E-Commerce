412200718d6f80fa47fbd5a16e1cbd31965acf9c
Abdias <abdiasejr@gmail.com>
2022-03-04 22:38:16 -0300

    new mocks

--- api/src/mockData/mockinventory.js
+++ api/src/mockData/mockinventory.js
@@ -23,6 +23,54 @@ const mockinventory = [
   {
     quantity: "1",
   },
+  {
+    quantity: "12",
+  },
+  {
+    quantity: "10",
+  },
+  {
+    quantity: "34",
+  },
+  {
+    quantity: "99",
+  },
+  {
+    quantity: "432",
+  },
+  {
+    quantity: "2",
+  },
+  {
+    quantity: "33",
+  },
+  {
+    quantity: "1",
+  },
+  {
+    quantity: "12",
+  },
+  {
+    quantity: "10",
+  },
+  {
+    quantity: "34",
+  },
+  {
+    quantity: "99",
+  },
+  {
+    quantity: "432",
+  },
+  {
+    quantity: "2",
+  },
+  {
+    quantity: "33",
+  },
+  {
+    quantity: "1",
+  },
 ];
 
 module.exports = {
