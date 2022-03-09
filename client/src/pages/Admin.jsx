e3a8ccdf4b0b405c326344c4feefdddd4de00a4e
f2000fernandez <f2000.fernandez@gmail.com>
2022-03-09 20:41:32 -0300

    Nav on admin

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { SideBar } from "../components";
-import { AdminContent, AddContainer } from "../containers";
+import { AdminContent, AddContainer, NavBar } from "../containers";
 
 const Admin = () => {
   const [option, setOption] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="admin-page--container">
+      <NavBar isScroll={true}/>
       <SideBar setOption={setOption} setIsOpen={setIsOpen} />
       {isOpen && option && <AddContainer option={option} setIsOpen={setIsOpen} /> }
       {!isOpen && <AdminContent option={option} setIsOpen={setIsOpen} />}
