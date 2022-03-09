e2a4fc11825ec4c3c523edaa0ef0a5fc0482dbe8
unknown <gross.franco@hotmail.com>
2022-03-09 17:03:07 -0300

    revert

--- client/src/components/StripeButton.jsx
+++ client/src/components/StripeButton.jsx
@@ -1,27 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import StripeCheckout from 'react-stripe-checkout';
 import { Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 
 //the test card to use on stripe is # 4242424242424242 expiration date needs to be in the future las number any 3
 
-const StripeButton = (subtotal) =>{
+const StripeButton = (subtotal) => {
     //set up products, below is just example, at the moment we can only take 1 product at a time for the back end to function properly, so the price in the state needs to be final
 
-    const[product, setProduct] = useState({
-        id: subtotal.products.map((product) =>{
+    const { user } = useSelector((state) => state.session);
+
+    const [product, setProduct] = useState({
+        id: subtotal.products.map((product) => {
             return product.id
         }),
-        quantity: subtotal.products.map((product) =>{
+        quantity: subtotal.products.map((product) => {
             return product.quantity
         }),
-        name: subtotal.products.map((product) =>{
+        name: subtotal.products.map((product) => {
             return product.name
         }).toString(),
         price: Math.floor(subtotal.subTotal),
-        description: 'All cart items'
+        description: 'All cart items',
+        userid: user ? user.id : null
     });
     //the token is automatically created by stripe we just need to call it
-    const makePayment = token =>{
+    const makePayment = token => {
         const body = {
             token,
             product
@@ -33,24 +37,24 @@ const StripeButton = (subtotal) =>{
             method: 'POST',
             headers: headers,
             body: JSON.stringify(body)
-        }).then(response =>{
+        }).then(response => {
             console.log(subtotal)
             console.log("RESPONSE", response)
-            const {status} = response;
+            const { status } = response;
             console.log("STATUS", status)
         })
-        .catch(err => console.log(err))
+            .catch(err => console.log(err))
     }
     console.log(process.env.REACT_APP_STRIPE_PK_KEY)
-    return(
+    return (
         //right now is the default button, but if you add a button component inside you can change its appearce and keep the functionality
-        <StripeCheckout stripeKey={process.env.REACT_APP_STRIPE_PK_KEY} 
-        token={makePayment} 
-        amount={product.price * 100}
-        shippingAddress
-        
+        <StripeCheckout stripeKey={process.env.REACT_APP_STRIPE_PK_KEY}
+            token={makePayment}
+            amount={product.price * 100}
+            shippingAddress
+
         >
-            <Button  variant="primary" width='500px'>Compra ahora</Button>
+            <Button variant="primary" width='500px'>Compra ahora</Button>
 
         </StripeCheckout>
     )
