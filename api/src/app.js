54a931ad767f7190c39378f8fdf69484e8953f2e
Franco Gross <86983794+Gross-Franco@users.noreply.github.com>
2022-03-08 23:37:41 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 const { setCookie } = require("./middlewares/utilities.js");
 
 server.name = "API";
-server.use(cors({ origin: DOMAIN, credentials: true }))
+server.use(cors())
 
 
 server.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -21,7 +21,7 @@ server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", DOMAIN); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
