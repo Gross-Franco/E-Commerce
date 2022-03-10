9c08f57cf1faf6d908077ea3cb9c4e2df394858c
unknown <gross.franco@hotmail.com>
2022-03-09 23:47:37 -0300

    hotfix buy from product details

--- client/src/components/StripeSingleItem.jsx
+++ client/src/components/StripeSingleItem.jsx
@@ -1,11 +1,12 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import StripeCheckout from 'react-stripe-checkout';
 import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { axiosWithCredentials as axios } from '../utilities/axios';
 
 //the test card to use on stripe is # 4242424242424242 expiration date needs to be in the future las number any 3
 
-const StripeSingleItem = () =>{
+const StripeSingleItem = () => {
     //set up products, below is just example, at the moment we can only take 1 product at a time for the back end to function properly, so the price in the state needs to be final
 
     const { productDetail } = useSelector((state) => state.products);
@@ -17,16 +18,16 @@ const StripeSingleItem = () =>{
     const productName = productDetail.name
     // console.log("PRODUCTID", productId)
 
-    const[product, setProduct] = useState({
+    const [product, setProduct] = useState({
         id: productId,
         quantity: 1,
         name: productName,
         price: Math.floor(productPrice),
         description: 'All cart items',
-        userid: user? user.id: null
+        userid: user ? user.id : null
     });
 
-    useEffect(() =>{
+    useEffect(() => {
         setProduct({
             id: productId,
             quantity: 1,
@@ -37,36 +38,30 @@ const StripeSingleItem = () =>{
     }, [productDetail, user])
     // console.log("PRODUCT IN STRIPE", product)
     //the token is automatically created by stripe we just need to call it
-    const makePayment = token =>{
+    const makePayment = token => {
         const body = {
             token,
             product
         }
-        const headers = {
-            'Content-Type': 'application/json'
-        }
-        return fetch(`http://localhost:3001/checkout/detail/payments1item`, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(body)
-        }).then(response =>{
-            // console.log(subtotal)
-            console.log("RESPONSE", response)
-            const {status} = response;
-            console.log("STATUS", status)
-        })
-        .catch(err => console.log(err))
+
+        axios.post(`/checkout/detail/payments1item`, body)
+            .then(response => {
+                console.log("RESPONSE", response)
+                const { status } = response;
+                console.log("STATUS", status)
+            })
+            .catch(err => console.log(err))
     }
     // console.log(process.env.REACT_APP_STRIPE_PK_KEY)
-    return(
+    return (
         //right now is the default button, but if you add a button component inside you can change its appearce and keep the functionality
-        <StripeCheckout stripeKey={process.env.REACT_APP_STRIPE_PK_KEY} 
-        token={makePayment} 
-        amount={product.price * 100}
-        shippingAddress
-        
+        <StripeCheckout stripeKey={process.env.REACT_APP_STRIPE_PK_KEY}
+            token={makePayment}
+            amount={product.price * 100}
+            shippingAddress
+
         >
-            <Button  variant="primary" width='500px'>Compra ahora</Button>
+            <Button variant="primary" width='500px'>Compra ahora</Button>
 
         </StripeCheckout>
     )
--- client/src/utilities/axios.js
+++ client/src/utilities/axios.js
@@ -12,35 +12,3 @@ export const axiosWithCredentials = axios.create({
     baseURL: process.env.REACT_APP_API || baseUrl,// change this to "https://pghenry.herokuapp.com/" to work with deplyed DB
     /* withCredentials: true, */
 })
-
-axiosWithCredentials.interceptors.response.use((response) => {
-    // if (response.data.isAdmin) {
-    //     store.dispatch(setAuthLevel(3));
-    // } else if (response.data.isUser) {
-    //     store.dispatch(setAuthLevel(2));
-    // } else if ((response.data.hasOwnProperty("isUser") && response.data.hasOwnProperty("isAdmin")) && (!response.data.isUser && !response.data.isAdmin)) {
-    //     store.dispatch(setAuthLevel(1));
-    // }
-    return response;
-}, (error) => {
-    let url = error.response.config.url.split("/")[1];
-    if (url === "api") {
-        // window.location.href = "/";
-    }
-    return error;
-})
-
-// axiosWithCredentials.interceptors.request.use((config) => {
-//     const url = config.url.split("/")[1];
-//     let level = 1;
-
-//     if (url === "admin") {
-//         level = 3;
-//     } else if (url === "user") {
-//         level = 2;
-//     }
-
-//     config.headers.common["Permits"] = `Level ${level}`;
-
-//     return config;
-// })
