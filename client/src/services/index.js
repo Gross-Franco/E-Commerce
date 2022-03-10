ac4d896e2e6ee9f6181fd74f3eea67e034c22395
Francisco Fernández <f2000.fernandez@gmail.com>
2022-03-09 21:12:34 -0300

    Update index.js

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -20,7 +20,7 @@ export const saveLocalStorage = (product) => {
       ? [...cart, { ...product, quantity: 1 }]
       : cart.map((item) => {
         if (item.id === product.id) {
-          if (item.quantity < product.inventory) return { ...item, quantity: item.quantity + 1 };
+          if (item.quantity <= product.inventory) return { ...item, quantity: item.quantity + 1 };
           else alert('Not enough stock')
         } else {
           return item;
